52b12f253fcfaee417e1326e72e249df51397053
LucasMartinCosta <lucassmartin.costa@gmail.com>
2024-11-01 09:37:28 -0300

    primer Commit

---
+++ src/app/recetas/receta-card/receta-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-receta-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './receta-card.component.html',
+  styleUrl: './receta-card.component.css'
+})
+export class RecetaCardComponent {
+
+}
---
+++ src/app/recetas/receta-list/receta-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-receta-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './receta-list.component.html',
+  styleUrl: './receta-list.component.css'
+})
+export class RecetaListComponent {
+
+}
